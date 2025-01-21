465f0deec7becd01ffe464150895888764559361
Thomas Butterworth <thomas.m.butterworth@gmail.com>
2025-01-21 18:35:32 +0000

    params wrapped in suspense (spooky)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,25 @@ import { useSearchParams } from "next/navigation";
 import { Bingo } from "@/components";
 import { LoadingSpinner, PageContainer } from "@/components/ui";
 import { Title } from "@/components/ui/Title";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Suspense } from "react";
 import { nightMap, NightTheme, NightType } from "@/utils";
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(true);
+  const searchParamsSuspense = (
+    <Suspense fallback={<LoadingSpinner />}>
+      <SearchParams />
+    </Suspense>
+  );
+
+  return <PageContainer>{searchParamsSuspense}</PageContainer>;
+}
+
+function SearchParams() {
   const searchParams = useSearchParams();
   const nightId = (searchParams.get("night") as NightType) || "scratch";
   const night: NightTheme = nightMap[nightId] || nightMap["scratch"];
+
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const timeout = setTimeout(() => setIsLoading(false), 800);
