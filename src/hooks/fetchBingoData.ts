b14ab7f80cb0a66dfd32ffd2c469182d84214d1a
Thomas Butterworth <thomas.m.butterworth@gmail.com>
2025-01-28 19:20:10 +0000

    api url hardcoded for now

--- src/hooks/fetchBingoData.ts
+++ src/hooks/fetchBingoData.ts
@@ -1,5 +1,5 @@
 export const fetchBingoData = async () => {
-  const gameUrl = `${process.env.GAMES_API}/bingo`;
+  const gameUrl = "https://nmitb-games-api.vercel.app/api/games/bingo";
   const gameFetch = await fetch(gameUrl);
   const gameData = gameFetch.json();
   return gameData;
